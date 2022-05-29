f2391ce173d7e7e3b67a43d9a12166eda496be77
hanuradiarjo <hanuradiarjo@gmail.com>
2022-05-29 13:36:15 +0700

    Video 9

--- react-hello-world/src/container/BlogPost/BlogPost.jsx
+++ react-hello-world/src/container/BlogPost/BlogPost.jsx
@@ -6,7 +6,97 @@ import axios from 'axios';
 
 class BlogPost extends Component{
     state ={
-        post: []
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        }, isUpdate: false
+    }
+
+    getPostAPI = () => {
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
+        .then((res) => {
+            this.setState({
+                post: res.data
+            })
+        })
+    }
+
+    postDataAPI = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                }, isUpdate: false
+
+            })
+        }, (err) => { console.log('error: ',err); })
+    }
+
+    putDataAPI = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then((res) => {
+            console.log(res);
+            this.getPostAPI();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                }, isUpdate: false
+
+            })
+        })
+    }
+
+    handleRemove = (data) => {
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res)=>{
+            console.log(res);
+            this.getPostAPI();
+        })
+    }
+
+    handleUpdate= (data) => {
+        console.log(data)
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+        // axios.put(`http://localhost:3004/posts/${data}`, this.state.formBlogPost).then((res) => {
+        //     console.log(res);
+        //     this.getPostAPI();
+        // })
+    }
+
+    handleFormChange =(event)=> {
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        if(!this.state.isUpdate){
+            formBlogPostNew['id'] = timestamp;
+        }
+        
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+        
+    }
+
+    handleSimpan = () => {
+        if(this.state.isUpdate){
+            this.putDataAPI()
+        } else{
+            this.postDataAPI();
+        }
+       
     }
 
     componentDidMount(){
@@ -17,21 +107,23 @@ class BlogPost extends Component{
         //         post: json
         //     })
         // })
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => {
-            this.setState({
-                post: res.data
-            })
-        })
+        this.getPostAPI();
     }
      
     render(){
         return(
             <Fragment>
                  <p className="section-title">Blog Post</p>
+                 <div className="form-add-post">
+                     <label htmlFor="header">Title</label>
+                     <input type="text" value={this.state.formBlogPost.title} name="title" placeholder="add title" onChange={this.handleFormChange}/>
+                     <label htmlFor="body">Blog Content</label>
+                     <textarea name="body" value={this.state.formBlogPost.body} id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+                     <button className="btn-simpan" onClick={this.handleSimpan}>Simpan</button>
+                 </div>
                  {
                      this.state.post.map(post => {
-                         return <Post key={post.id} title={post.title} desc={post.body}/>
+                         return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUpdate}/>
                      })
                  }
                  
